16b993a82a0ee78580bec9bf6a8d60ad4b708c44
Askhad Arzybaev <aarzybaev@gmail.com>
2023-01-21 15:33:22 +0600

    feat: solution

--- src/index.js
+++ src/index.js
@@ -37,9 +37,26 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+function chunkString (str, len) {
+    const size = str.length/len;
+    const r = Array(size);
+    let offset = 0;
+    
+    for (let i = 0; i < size; i++) {
+      r[i] = str.substr(offset, len);
+      offset += len;
+    }
+    
+    return r;
+  }
+
 function decode(expr) {
-    // write your solution here
+    let chunked = chunkString(expr, 10);
+    return chunked.reduce((acc, curr) => acc += (curr != '**********') ?  MORSE_TABLE[curr.replace(/10/g, '.').replace(/11/g, '-').replace(/0/g, '')] : ' ', '');
 }
+
+
 
 module.exports = {
     decode
